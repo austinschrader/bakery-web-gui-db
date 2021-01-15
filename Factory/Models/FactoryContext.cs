465842000dc83cac9a3023111d3a95b6a7cb61f6
Austin Schrader <austin.jschrader@gmail.com>
2021-01-15 14:08:06 -0800

    Update FactoryContext to extend Identity

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Models
 {
-  public class FactoryContext : DbContext
+  public class FactoryContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Engineer> Engineers { get; set; }
     public virtual DbSet<Machine> Machines { get; set; }
