c26ee3012e73bbac4cc70ca9c8d42121080c984a
Austin Schrader <austin.jschrader@gmail.com>
2021-01-18 01:56:09 -0800

    Add first password field

--- Factory/ViewModels/RegisterViewModel.cs
+++ Factory/ViewModels/RegisterViewModel.cs
@@ -7,5 +7,13 @@ namespace Factory.ViewModels
     [Required]
     [EmailAddress]
     [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+
   }
 }
