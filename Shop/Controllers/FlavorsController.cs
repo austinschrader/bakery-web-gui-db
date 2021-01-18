df1ddb41bb87ce6a82e86fd13fc3946146b5188c
Austin Schrader <austin.jschrader@gmail.com>
2021-01-18 03:57:26 -0800

    Update access to flavors

--- Shop/Controllers/FlavorsController.cs
+++ Shop/Controllers/FlavorsController.cs
@@ -5,15 +5,22 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Shop.Controllers
 {
+  [Authorize]
   public class FlavorsController : Controller
   {
     private readonly ShopContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FlavorsController(ShopContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager, ShopContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
